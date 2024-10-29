9b3c496d8c37de0534644d0e10db390e550beb33
alope0420 <alexander@opedal.org>
2024-10-29 23:24:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+import {LocalStorage} from 'node-localstorage';
+import fsp from 'fs/promises';
+
+const localStorage = new LocalStorage('./storage');
+
+const app = express();
+
+app.set('views', path.join(import.meta.dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(import.meta.dirname, 'static')));
+
+app.use(express.json());
+
+const allWords = JSON.parse(await fsp.readFile('words.json'));
+
+function shuffle(array) {
+    let i = array.length;
+    while (i) {
+        let j = Math.floor(Math.random() * i--);
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+app.post('/submit', (req, res) => {
+    if (req.body === null || typeof req.body !== 'object' || Array.isArray(req.body) || !Object.keys(req.body).length) {
+        res.status(400).send('not ok');
+        return;
+    }
+
+    console.log('inserting');
+
+    let responses = JSON.parse(localStorage.getItem('responses')) ?? [];
+    responses.push({...req.body, _resp_id: responses.length + 1});
+    console.log('responses', responses);
+    localStorage.setItem('responses', JSON.stringify(responses));
+
+    res.status(200).send('ok');
+});
+
+app.get('/responses', (req, res) => {
+    const responses = JSON.parse(localStorage.getItem('responses')) ?? [];
+    const columns = [...new Set(responses.flatMap(response => Object.keys(response)))].sort();
+    const rows = responses.map(response => columns.map(col => response[col]));
+    res.render('responses', {columns, rows});
+});
+
+app.get('/:questionLimit?', (req, res) => {
+    
+    const limit = req.params.questionLimit ?? allWords.length;
+    const wordSet = allWords.slice(0, limit);
+
+    let questions = ['comic', 'arial'].flatMap(font =>
+        wordSet.map((words, index) => ({
+            id: `${index + 1}_${font}`,
+            words: shuffle(words),
+            targetWord: words[Math.floor(Math.random() * words.length)],
+            font,
+        }))
+    );
+
+    shuffle(questions);
+
+    res.render('survey', {questions});
+});
+
+app.listen(81, 'localhost');
---
+++ static/script.js
@@ -0,0 +1,52 @@
+
+let time = Date.now();
+let response = {};
+
+function startSurvey() {
+    $('#intro-message').addClass('d-none');
+    $('#progress').removeClass('d-none');
+    activateQuestion(0);
+}
+
+function activateQuestion(questionIndex) {
+    questionIndex = parseInt(questionIndex);
+    $(`#question-${questionIndex}`).removeClass('d-none');
+    $('#current-question').text(questionIndex + 1);
+    time = Date.now();
+}
+
+function deactivateQuestion(questionIndex, answeredCorrectly) {
+    questionIndex = parseInt(questionIndex);
+    $(`#question-${questionIndex}`).addClass('d-none');
+    response['q' + $(`#question-${questionIndex}`).data().questionId + '_correct'] = answeredCorrectly;
+    response['q' + $(`#question-${questionIndex}`).data().questionId + '_time'] = Date.now() - time;
+}
+
+function chooseOption(option) {
+    option = $(option);
+    const parent = option.closest('.question');
+    deactivateQuestion(parent.data().thisQuestion, option.data().correctOption);
+
+    // Increment relevant counter
+    const counterId = (option.data().correctOption ? 'correct' : 'wrong');
+    const counter = parseInt($(`#${counterId}-answers`).text()) + 1;
+    $(`#${counterId}-answers`).text(counter);
+
+    if (parent.data().lastQuestion) {
+        console.log(response);
+        submitResponse(response);
+        $('#exit-message').removeClass('d-none');
+    } else {
+        activateQuestion(parent.data().nextQuestion);
+    }
+}
+
+function submitResponse(response) {
+    fetch('/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(response),
+    })
+}
