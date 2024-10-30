3420ac0ecf7643a6fc24f9019cc02660004f183c
alope0420 <alexander@opedal.org>
2024-10-30 01:06:46 +0100

    split routes into separate file

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
-import fsp from 'fs/promises';
 import process from 'process'
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Redis } from '@upstash/redis'
-
-const redis = new Redis({
+import {Redis} from '@upstash/redis';
+global.redis = new Redis({
     url: process.env.KV_REST_API_URL,
     token: process.env.KV_REST_API_TOKEN,
 });
+
+import router from './routers/main.js';
 
 const app = express();
 
@@ -23,52 +23,6 @@ app.use(express.static(path.join(process.cwd(), 'static')));
 
 app.use(express.json());
 
-const allWords = JSON.parse(await fsp.readFile(path.join(process.cwd(), 'words.json')));
-
-function shuffle(array) {
-    let i = array.length;
-    while (i) {
-        let j = Math.floor(Math.random() * i--);
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-
-app.post('/submit', async (req, res) => {
-    if (req.body === null || typeof req.body !== 'object' || Array.isArray(req.body) || !Object.keys(req.body).length) {
-        res.status(400).send('not ok');
-        return;
-    }
-
-    await redis.lpush('responses', {...req.body, _resp_id: await redis.llen('responses') + 1});
-
-    res.status(200).send('ok');
-});
-
-app.get('/responses', async (req, res) => {
-    const responses = await redis.lrange('responses', 0, -1);
-    const columns = [...new Set(responses.flatMap(response => Object.keys(response)))].sort();
-    const rows = responses.map(response => columns.map(col => response[col]));
-    res.render('responses', {columns, rows});
-});
-
-app.get('/', (req, res) => {
-    
-    const limit = req.query.questionLimit ?? allWords.length;
-    const wordSet = allWords.slice(0, limit);
-
-    let questions = ['comic', 'arial'].flatMap(font =>
-        wordSet.map((words, index) => ({
-            id: `${index + 1}_${font}`,
-            words: shuffle(words),
-            targetWord: words[Math.floor(Math.random() * words.length)],
-            font,
-        }))
-    );
-
-    shuffle(questions);
-
-    res.render('survey', {questions});
-});
+app.use(router);
 
 app.listen(81, 'localhost');
---
+++ routers/main.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import fsp from 'fs/promises';
+import path from 'path';
+
+const router = express.Router();
+export default router;
+
+// Read all word sets from file
+const allWords = JSON.parse(await fsp.readFile(path.join(process.cwd(), 'words.json')));
+
+// Standard Fisher-Yates shuffle
+function shuffle(array) {
+    let i = array.length;
+    while (i) {
+        let j = Math.floor(Math.random() * i--);
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+router.post('/submit', async (req, res) => {
+    // Don't add empty responses to db
+    if (req.body === null || typeof req.body !== 'object' || Array.isArray(req.body) || !Object.keys(req.body).length) {
+        res.status(400).send('not ok');
+        return;
+    }
+
+    // Push new response to redis
+    await redis.lpush('responses', {...req.body, _resp_id: await redis.llen('responses') + 1});
+
+    res.status(200).send('ok');
+});
+
+router.get('/responses', async (req, res) => {
+    // Get responses as array from redis
+    const responses = await redis.lrange('responses', 0, -1);
+
+    // Find all unique columns across all existing responses
+    const columns = [...new Set(responses.flatMap(response => Object.keys(response)))].sort();
+
+    // Build rows from column names (allowing columns to be empty)
+    const rows = responses.map(response => columns.map(col => response[col]));
+
+    // Render responses as table
+    res.render('responses', {columns, rows});
+});
+
+router.get('/', (req, res) => {
+    
+    // If a question limit is specified, we only take the first X sets of words
+    const limit = req.query.questionLimit ?? allWords.length;
+    const wordSet = allWords.slice(0, limit);
+
+    // Make 1 question per font per word list
+    let questions = ['comic', 'arial'].flatMap(font =>
+        wordSet.map((words, index) => ({
+            id: `${index + 1}_${font}`, // Unique ID based on word list index + font name
+            words: shuffle(words), // Shuffle word list randomly every time
+            targetWord: words[Math.floor(Math.random() * words.length)], // Always pick random target word
+            font,
+        }))
+    );
+
+    // Shuffle actual question order
+    shuffle(questions);
+
+    res.render('survey', {questions});
+});
